449b3885d0f743bebbf0ae4044a47d5e2215ab63
DEE <donzydonathan@gmail.com>
2023-07-04 14:19:03 +0100

    chore: add colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-violet": "hsl(235, 24%, 19%)",
+        "gradient-1": "hsl(192, 100%, 67%)",
+        "gradient-2": "hsl(280, 87%, 65%)",
+        "element-violet": "hsl(233, 14%, 35%)",
+        "active-blue": "hsl(220, 98%, 61%)",
+        "shadow-color": "hsl(235, 24%, 19%)",
+      },
+    },
   },
   plugins: [],
 };
