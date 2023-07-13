d03c69a4510849d118378994215c53252cfacc31
DEE <donzydonathan@gmail.com>
2023-07-13 12:30:42 +0100

    feat: complete new custom input and checkbox components

--- components/CustomCheckBoxCopy.tsx
+++ components/CustomCheckBoxCopy.tsx
@@ -1,9 +1,27 @@
 import { View, Text } from "react-native";
+import Checkbox from "expo-checkbox";
+import { LinearGradient } from "expo-linear-gradient";
+import { useState } from "react";
 
 export default function CustomCheckBox() {
+  const [isChecked, setIsChecked] = useState(false);
   return (
-    <View>
-      <Text className="text-white">checkbox</Text>
-    </View>
+    <LinearGradient
+      className="rounded-xl"
+      colors={
+        isChecked
+          ? ["hsl(192, 100%, 67%)", "hsl(280, 87%, 65%)"]
+          : ["transparent", "transparent"]
+      }
+      start={{ x: 0.0, y: 0.0 }}
+      end={{ x: 1, y: 0 }}
+    >
+      <Checkbox
+        className="rounded-xl p-2.5"
+        value={isChecked}
+        onValueChange={setIsChecked}
+        color={isChecked ? "transparent" : undefined}
+      />
+    </LinearGradient>
   );
 }
--- components/InputCopy.tsx
+++ components/InputCopy.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TextInput } from "react-native";
+import { View, TextInput } from "react-native";
 import { useState } from "react";
 import CustomCheckBox from "./CustomCheckBoxCopy";
 
@@ -7,14 +7,14 @@ export default function Input() {
 
   return (
     <View
-      className="flex-row rounded-md bg-element-violet overflow-hidden"
+      className="flex-row rounded-md bg-element-violet overflow-hidden py-2"
       style={{ width: "85%" }}
     >
       <View style={{ flex: 0.15 }} className="items-center justify-center p-2">
         <CustomCheckBox />
       </View>
       <TextInput
-        className="p-2"
+        className="p-2 text-white"
         style={{ flex: 0.85 }}
         value={task}
         onChangeText={setNewTask}
