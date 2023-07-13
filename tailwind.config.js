8145a9caf9795701ac332c17a1e7d5ea5d6891da
DEE <donzydonathan@gmail.com>
2023-07-13 10:19:06 +0100

    feat: add custom colors back to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-violet": "hsl(235, 21%, 11%)",
+        "gradient-1": "hsl(192, 100%, 67%)",
+        "gradient-2": "hsl(280, 87%, 65%)",
+        "element-violet": "hsl(235, 24%, 19%)",
+        "element-violet-2": "hsl(237, 14%, 26%)",
+        "active-blue": "hsl(220, 98%, 61%)",
+        "shadow-color": "hsl(235, 24%, 19%)",
+      },
+    },
   },
   plugins: [],
 };
