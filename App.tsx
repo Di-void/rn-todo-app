07e0c7b7eaf14cd0389617ed2d5389dd50730800
DEE <donzydonathan@gmail.com>
2023-07-13 03:05:08 +0100

    chore: add boilerplate

---
+++ App.tsx
@@ -0,0 +1,27 @@
+import ScreenWrapper from "./components/ScreenWrapper";
+import Tabs from "./components/Tabs";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  useFonts,
+  JosefinSans_500Medium,
+} from "@expo-google-fonts/josefin-sans";
+
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    JosefinSans_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <SafeAreaView className="bg-bg-violet h-full">
+      <ScreenWrapper>
+        <Tabs />
+      </ScreenWrapper>
+      <StatusBar style="light" />
+    </SafeAreaView>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 export default function IndexPage() {
   return (
     <View className="bg-red-200">
-      <Text className="text-red-900">Hello, world!</Text>
+      <Text className="text-red-900 text-3xl">Hello, world!</Text>
     </View>
   );
 }
