80f8ec791d65be28db66bbfb9c5b57fc8ee2e092
DEE <donzydonathan@gmail.com>
2023-07-25 14:52:21 +0100

    feat,chore: add custom splash screen and icon

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,24 +2,47 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { View } from "react-native";
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import * as Font from "expo-font";
 import Header from "../components/Header";
 import TabBar from "../components/TabBar";
-import {
-  useFonts,
-  JosefinSans_500Medium,
-} from "@expo-google-fonts/josefin-sans";
+import { JosefinSans_500Medium } from "@expo-google-fonts/josefin-sans";
+import * as SplashScreen from "expo-splash-screen";
+import { useState, useEffect, useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function AppLayout() {
-  let [fontsLoaded] = useFonts({
-    JosefinSans_500Medium,
-  });
+  const [appIsReady, setAppIsReady] = useState(false);
 
-  if (!fontsLoaded) {
+  useEffect(() => {
+    (async () => {
+      try {
+        await Font.loadAsync({ JosefinSans_500Medium });
+      } catch (e) {
+        // handle error
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    })();
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
     return null;
   }
 
   return (
-    <SafeAreaView className="bg-bg-violet" style={{ flex: 1 }}>
+    <SafeAreaView
+      className="bg-bg-violet"
+      style={{ flex: 1 }}
+      onLayout={onLayout}
+    >
       <Header />
       <View style={{ bottom: 20, maxHeight: "50%" }} className="items-center">
         <Slot />
